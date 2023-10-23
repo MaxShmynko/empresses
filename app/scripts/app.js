7d8a5165f0e21431ded9b68000b136a2999dcfb8
Alexander Chernyh <alexchernyh2000@mail.ru>
2023-10-23 17:49:32 +0300

    add animation

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,11 +1,17 @@
 import $ from "jquery";
+import WOW from "wowjs";
 import { DATA } from "./data";
 import { Test } from "./test";
 
 const test = new Test(DATA);
 
 $(() => {
+	const wow = new WOW.WOW({
+		live: false,
+		boxClass: "wow",
+	});
 
+	wow.init();
 	$(document).ready(function() {
 		// Находим кнопку по классу promo__test и добавляем обработчик события click
 		$(".promo__test").click(function() {
